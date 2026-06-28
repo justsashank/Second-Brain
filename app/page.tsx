5245f2b7585fa449d1496add2ac7787e4b190783
justsashank <sashankla05@gmail.com>
2026-06-28 23:56:55 +0530

    changes made

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import LoginPage from "./components/LoginPage";
+import Dashboard from "./components/Dashboard";
+import Sidebar from "./components/Sidebar";
 
 
 export default function Page(){
 
   return (
-
-    <LoginPage />
+    <>
+      <LoginPage />
+      <Dashboard />
+      <Sidebar />
+    </>
 
   );
 
